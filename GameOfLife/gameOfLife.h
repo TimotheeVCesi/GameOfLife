09252fd14f60da3cdf5c011e7df320146de89d69
TimotheeVCesi <timothee.viriot@viacesi.fr>
2024-12-08 01:25:02 +0100

    amélioration méthode run

--- GameOfLife/gameOfLife.h
+++ GameOfLife/gameOfLife.h
@@ -1,24 +1,38 @@
 #ifndef GOF_H
 #define GOF_H
 
+#include "headers.h"
+#include "file.h"
 #include "grid.h"
 #include "view.h"
 #include "iteration.h"
 
 class GameOfLife {
 private:
+    IFileHandler* fileHandler;
     IGrid* grid;
     IView* view;
     IIteration* iterations;
+    int viewType;
 
 public:
-    GameOfLife(IGrid* grid, IView* view, IIteration* iterations) : grid(grid), view(view) {}
+    GameOfLife(IFileHandler* fileHandler, IGrid* grid, IView* view, IIteration* iterations, int viewType)
+                : fileHandler(fileHandler), grid(grid), view(view), iterations(iterations), viewType(viewType) {}
 
     void run(int sleepTime) {
+        grid = fileHandler->load();
         while (iterations->canContinue(*grid)) {
-            view->display(*grid);
+            if (viewType == 1) {
+                fileHandler->save(*grid);
+                std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
+            } else if (viewType == 2) {
+                int cellSize = 10;
+                sf::RenderWindow window(sf::VideoMode(grid->getColumns() * cellSize, grid->getRows() * cellSize), "Game of Life");
+
+                view->display(*grid, window, cellSize);
+                sf::sleep(sf::milliseconds(sleepTime));
+            }
             grid->update();
-            std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
         }
     }
 };
