836c5e117c6dc04f50d0a3c41ca0a07470683dbe
TimotheeVCesi <timothee.viriot@viacesi.fr>
2024-12-08 01:24:08 +0100

    changement utilisation view display (parametres)

--- GameOfLife/view.h
+++ GameOfLife/view.h
@@ -4,18 +4,12 @@
 class IView {
 public:
     virtual ~IView() = default;
-    virtual void display(const IGrid& grid) const = 0;
+    virtual void display(const IGrid& grid, sf::RenderWindow& window, int cellSize) const = 0;
 };
 
 class ViewGraphic : public IView {
-private:
-    sf::RenderWindow& window;
-    float cellSize;
-
 public:
-    ViewGraphic(sf::RenderWindow& win, int cellSize) : window(win), cellSize(cellSize) {}
-
-    void display(const IGrid& grid) const override {
+    void display(const IGrid& grid, sf::RenderWindow& window, int cellSize) const override {
         window.clear();
         sf::RectangleShape cell(sf::Vector2f(cellSize - 1.0f, cellSize - 1.0f));
 
